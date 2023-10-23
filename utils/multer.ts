f3be01979ef2efd01832534544b5323d6f88ea3a
jenan <jenan.hong@gmail.com>
2023-10-23 21:51:52 +0800

    fix: remove upload feature.

--- utils/multer.ts
+++
@@ -1,9 +0,0 @@
-const multer = require('multer');
-
-// Define storage for uploaded files
-const storage = multer.memoryStorage();
-
-// Create the multer instance with your storage options
-const upload = multer({ storage: storage });
-
-module.exports = upload;
